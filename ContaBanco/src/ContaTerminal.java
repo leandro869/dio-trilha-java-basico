9746437c6d9d88697f2219693343b92540b062b3
Leandro Henrique <leandro.h22@hotmail.com>
2025-03-22 21:42:00 -0300

    Desafio Concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+import java.util.Locale;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US); // Garante que o separador decimal seja ponto (.)
+        
+        //Exibir as Mensagens para o nosso usuário
+        System.out.println("Por favor, digite o número da Agência !");
+        //Obter pela scanner os valores digitados no terminal
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+        
+        //Exibir as Mensagens para o nosso usuário
+        System.out.println("Por favor, digite a Agência !");
+        //Obter pela scanner os valores digitados no terminal
+        String agencia = scanner.nextLine();
+        
+        //Exibir as Mensagens para o nosso usuário
+        System.out.println("Por favor, digite o Nome do Cliente");
+        //Obter pela scanner os valores digitados no terminal
+        String nomeCliente = scanner.nextLine();
+        
+        //Exibir as Mensagens para o nosso usuário
+        System.out.println("Por favor, digite o Saldo");
+        //Obter pela scanner os valores digitados no terminal
+        double saldo = scanner.nextDouble();
+        
+        //Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+                + agencia + ", conta " + numero + " e seu saldo " + String.format("%.2f", saldo) 
+                + " já está disponível para saque.");
     }
 }
